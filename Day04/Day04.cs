71322064b3877780918485a46890a59a406dfed3
David Darj <david.darj@saabgroup.com>
2025-12-04 13:02:29 +0100

    Day 04 part 1 solved

---
+++ Day04/Day04.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+
+namespace AoC
+{
+    public static class Day04
+    {
+        static readonly (int,int)[] directions = {
+            ( -1, -1 ), ( 0, -1 ), ( 1, -1 ),
+            ( -1, 0 ), ( 0, 0 ), ( 1, 0 ),
+            ( -1, 1 ), ( 0, 1 ), ( 1, 1 ),
+        };
+
+        public static Int64 Day04a(string[] input)
+        {
+            Int64 result = 0;
+            for (var row = 0; row != input.Length; ++row)
+            {
+                string s = "";
+                for (var col = 0; col != input[row].Length; ++col)
+                {
+                    if (input[row][col] == '@')
+                    {
+                        int neighbours = 0;
+                        foreach (var dir in directions)
+                        {
+                            var r = row + dir.Item2;
+                            var c = col + dir.Item1;
+                            if (r >= 0 && r < input.Length && 
+                                c >= 0 && c < input[r].Length &&
+                                input[r][c] == '@') neighbours++;
+                        }
+                        if (neighbours <= 4) // Include itself
+                        {
+                            result++;
+                            s+='x';
+                        }
+                        else
+                            s+=input[row][col];
+                    }
+                    else
+                        s += input[row][col];
+                }
+                Console.WriteLine(s);
+            }
+            return result;
+        }
+
+        public static Int64 Day04b(string[] input)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day04.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day04a : {0}   Time: {1}", Day04a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day04b : {0}   Time: {1}", Day04b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day04/Day04Test.cs
@@ -0,0 +1,58 @@
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC
+{
+    public class Day04Test
+    {
+        readonly string input =
+@"..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+";
+
+        readonly Int64 resultA = 13;
+        readonly Int64 resultB = 0;
+
+        [Fact]
+        public void Day04a()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day04.Day04a(lines);
+            Assert.Equal(resultA, result);
+
+            Console.WriteLine("Day04a : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+
+        [Fact]
+        public void Day04b()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day04.Day04b(lines);
+            Assert.Equal(resultB, result);
+
+            Console.WriteLine("Day04b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+        public Day04Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
