68ebd526316d0eaa6e7714a97a4c1c8bb788475a
David Darj <zid@alagazam.net>
2025-12-07 17:33:20 +0100

    Day 07 part 1 solved

---
+++ Day07/Day07.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+
+namespace AoC
+{
+    public static class Day07
+    {
+        public static Int64 Day07a(string[] input)
+        {
+            char[] beams = input[0].ToArray();
+            Int64 result = 0;
+            foreach (var i in input)
+            { 
+                foreach (var (c, n) in i.Select((c, n) => (c, n)))
+                {
+                    if (c == 'S') beams[n] = '|';
+                    else if  (c == '^' && beams[n] == '|')
+                    {
+                        ++result;
+                        beams[n - 1] = '|';
+                        beams[n] = '.';
+                        beams[n + 1] = '|';
+                    }
+                }
+            }
+            return result;
+        }
+
+        public static Int64 Day07b(string[] input)
+        {
+            return 0;
+        }
+
+
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("Day07.txt");
+            var sw = Stopwatch.StartNew();
+            Console.WriteLine("Day07a : {0}   Time: {1}", Day07a(lines), sw.ElapsedMilliseconds);
+            sw.Restart();
+            Console.WriteLine("Day07b : {0}   Time: {1}", Day07b(lines), sw.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Day07/Day07Test.cs
@@ -0,0 +1,64 @@
+using System.Diagnostics;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AoC
+{
+    public class Day07Test
+    {
+        readonly string input =
+@".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+";
+
+        readonly Int64 resultA = 21;
+        readonly Int64 resultB = 0;
+
+        [Fact]
+        public void Day07a()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day07.Day07a(lines);
+            Assert.Equal(resultA, result);
+
+            Console.WriteLine("Day07a : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+
+        [Fact]
+        public void Day07b()
+        {
+            var sw = Stopwatch.StartNew();
+            var lines = input.Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = Day07.Day07b(lines);
+            Assert.Equal(resultB, result);
+
+            Console.WriteLine("Day07b : {0}   Time: {1}", result, sw.ElapsedMilliseconds);
+        }
+
+        public Day07Test(ITestOutputHelper output)
+        {
+            var converter = new AoCUtils.Converter(output);
+            Console.SetOut(converter);
+        }
+
+    }
+
+}
